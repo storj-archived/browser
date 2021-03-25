7d0c50de9e03d9a5e0badf051991fa83dd0b365f
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-03-25 13:52:47 -0400

    Moved the file location inline.

--- src/components/FileEntry.vue
+++ src/components/FileEntry.vue
@@ -110,7 +110,13 @@
 			<span v-if="file.type === 'file'">{{ uploadDate }}</span>
 		</td>
 		<td class="text-right">
-			<div v-if="file.type === 'file'">
+			<div v-if="file.type === 'file'" class="d-inline-flex">
+				<button class="btn btn-white btn-actions" v-on:click="fileLocation">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+						<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+						<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+					</svg>
+				</button>
 				<div class="dropleft">
 					<div
 						v-if="loadingSpinner()"
@@ -152,8 +158,8 @@
 							v-on:click="download"
 						>
 							<svg
-								width="1.5em"
-								height="1.5em"
+								width="1.2em"
+								height="1.2em"
 								viewBox="0 0 16 16"
 								class="bi bi-cloud-download mr-2 ml-1"
 								fill="currentColor"
@@ -175,8 +181,8 @@
 							v-on:click="share"
 						>
 							<svg
-								width="2em"
-								height="2em"
+								width="1.5em"
+								height="1.5em"
 								viewBox="0 0 16 16"
 								class="bi bi-link-45deg mr-1"
 								fill="currentColor"
@@ -197,8 +203,8 @@
 							v-on:click="confirmDeletion"
 						>
 							<svg
-								width="2em"
-								height="2em"
+								width="1.5em"
+								height="1.5em"
 								viewBox="0 0 16 16"
 								class="bi bi-x mr-1"
 								fill="currentColor"
@@ -243,8 +249,8 @@
 									v-on:click="cancelDeletion"
 								>
 									<svg
-										width="2em"
-										height="2em"
+										width="1.5em"
+										height="1.5em"
 										viewBox="0 0 16 16"
 										class="bi bi-x mr-1"
 										fill="green"
@@ -306,8 +312,8 @@
 							v-on:click="confirmDeletion"
 						>
 							<svg
-								width="2em"
-								height="2em"
+								width="1.5em"
+								height="1.5em"
 								viewBox="0 0 16 16"
 								class="bi bi-x mr-1"
 								fill="currentColor"
@@ -352,8 +358,8 @@
 									v-on:click="cancelDeletion"
 								>
 									<svg
-										width="2em"
-										height="2em"
+										width="1.5em"
+										height="1.5em"
 										viewBox="0 0 16 16"
 										class="bi bi-x mr-1"
 										fill="green"
@@ -412,6 +418,11 @@ export default {
 	},
 
 	methods: {
+		fileLocation(e) {
+ 			e.stopPropagation();
+ 			this.$store.dispatch("files/openDropdown", null);
+ 			window.open("https://link.staging.tardigradeshare.io/s/jvgmjntaucpfedohxn3ogdrsfcfa/homepage/TardigradeExplainerVideo.m4v?map=1");
+ 		},
 		loadingSpinner() {
 			return !!this.$store.state.files.filesToBeDeleted.find(
 				file => file === this.file
