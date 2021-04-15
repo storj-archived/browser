ece653158c7a57663b6a80417b5e189a4fef3194
Monty Anderson <work@montyanderson.net>
2021-04-15 15:43:53 +0100

    fix sidebar opener

--- src/store/files.js
+++ src/store/files.js
@@ -322,7 +322,7 @@ export default {
 					if (uploadedFiles.length === 1) {
 						const [{ Key }] = uploadedFiles;
 
-						commit("files/openModal", state.path + Key);
+						commit("openModal", state.path + Key);
 					}
 
 					commit("finishUpload", params.Key);
