89705a0c6aaf6cd39e548ed9a45fc0ad2cae158b
Tome Boshevski <tome@storj.io>
2021-06-09 17:48:41 +0200

    Update FileBrowser.vue

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -10,7 +10,7 @@
 	-webkit-user-select: none;
 	-o-user-select: none;
 }
-
+f
 tbody {
 	user-select: none;
 }
@@ -37,6 +37,7 @@ tbody {
 	border-radius: 10px;
 	padding: 80px 20px;
 	background: #fafafb;
+	cursor: pointer;
 }
 
 .metric {
@@ -53,263 +54,272 @@ tbody {
 	border-color: #fe5d5d !important;
 	outline: none !important;
 }
-.back-route {
-	padding-right: 100%;
+
+.new-folder-row:hover {
+	background: #fff;
 }
-.back-route td {
-	border-top: none;
+
+.btn {
+  line-height: 2.4;
+}
+.btn-primary {
+  background: #376fff;
+	border-color: #376fff;
+}
+.btn-primary:hover {
+  background: #0047FF;
+  border-color: #0047FF;
+}
+.btn-light {
+	background: #e6e9ef;
+	border-color: #e6e9ef;
+}
+.btn-primary.disabled, .btn-primary:disabled {
+  color: #fff;
+  background-color: #001030;
+  border-color: #001030;
+}
+.input-folder {
+	height: 43px;
+}
+.drop-files-text {
+	font-weight: bold;
+	font-size: 18px;
 }
 </style>
 
 <template>
-	<div class="card border-0 p-4 p-lg-5" v-on:click="closeModalDropdown">
-		<div
-			class="div-responsive py-3"
-			v-cloak
-			@drop.prevent="upload"
-			@dragover.prevent
-		>
-			<div class="row mb-2">
-				<div class="col-sm-12 col-md-4 col-xl-8 mb-3">
-					<h2>
-						{{ bucketName }}
-						<!-- <span class="trash-can mx-1" v-if="areThereFilesToDelete()" v-on:click="deleteSelected">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-							<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-						</svg>
-					</span> -->
-					</h2>
-					<bread-crumbs></bread-crumbs>
-				</div>
-
-				<div class="col-sm-12 col-md-4 col-xl-2 mb-3">
-					<input
-						ref="fileInput"
-						type="file"
-						hidden
-						multiple
-						v-on:change="upload"
-					/>
-					<button
-						class="btn btn-primary btn-block"
-						v-on:click="buttonUpload"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							fill="currentColor"
-							class="bi bi-cloud-upload mr-2"
-							viewBox="0 0 16 16"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"
-							/>
-							<path
-								fill-rule="evenodd"
-								d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z"
-							/>
-						</svg>
-						Upload File
-					</button>
-				</div>
-
-				<div class="col-sm-12 col-md-4 col-xl-2 mb-3">
-					<button
-						class="btn btn-light btn-block"
-						v-on:click="toggleFolderCreationInput"
-						style="margin-right: 15px"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							fill="currentColor"
-							class="bi bi-folder-plus mr-2"
-							viewBox="0 0 16 16"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M9.828 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91H9v1H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0 0 13.81 4H9.828zm-2.95-1.707L7.587 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 0 1 1-.98h3.672a1 1 0 0 1 .707.293z"
-							/>
-							<path
-								fill-rule="evenodd"
-								d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"
-							/>
-						</svg>
-						New Folder
-					</button>
-				</div>
-			</div>
-
-			<div>
-				<table class="table table-hover">
-					<file-browser-header></file-browser-header>
-
-					<tbody>
-						<tr
-							v-for="file in filesUploading"
-							v-bind:key="file.ETag"
-							scope="row"
-						>
-							<td class="upload-text">
-								<span>
-									<svg
-										width="1em"
-										height="1em"
-										viewBox="0 0 16 16"
-										fill="currentColor"
-										xmlns="http://www.w3.org/2000/svg"
-										class="bi bi-file-earmark ml-2 mr-1"
-									>
-										<path
-											d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"
-										></path>
-										<path
-											d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"
-										></path>
-									</svg>
-									{{ filename(file) }}
-								</span>
-							</td>
-							<td>
-								<div class="progress">
-									<div
-										class="progress-bar"
-										role="progressbar"
-										v-bind:style="{
-											width: `${file.progress}%`
-										}"
-									>
-										{{ file.progress }}%
-									</div>
-								</div>
-							</td>
-							<td></td>
-							<td>
-								<button
-									class="btn btn-danger"
-									v-on:click="cancelUpload(file.Key)"
-								>
-									Cancel
-								</button>
-							</td>
-						</tr>
-
-						<tr v-if="path.length > 0">
-
-							<router-link to="../" class="back-route">
-										<td class="px-3">
-									<a
-										href="javascript:null"
-										v-on:click="back"
-										>..</a
-									>
-								</td>
-							</router-link>
-						</tr>
-
-						<tr
-							v-if="
-								$store.state.files
-									.createFolderInputShow === true
-							"
-						>
-							<td span="3">
-								<input
-									class="form-control ml-3"
-									v-bind:class="{
-										'folder-input':
-											createFolderInput.length >
-												0 &&
-											!createFolderEnabled
-									}"
-									type="text"
-									placeholder="Name of the folder"
-									v-model="createFolderInput"
-									v-on:keypress.enter="createFolder"
-								/>
-							</td>
-							<td span="3"></td>
-							<td span="3">
-								<button
-									v-on:click="createFolder"
-									v-bind:disabled="
-										!createFolderEnabled
-									"
-									class="btn btn-primary"
-								>
-									Save Folder
-								</button>
-								<span class="mx-1"></span>
-								<button
-									class="btn btn-light"
-									v-on:click="cancelFolderCreation"
-								>
-									Cancel
-								</button>
-							</td>
-							<td span="3">
-								<div
-									v-if="creatingFolderSpinner"
-									class="spinner-border"
-									role="status"
-								></div>
-							</td>
-						</tr>
-
-						<file-entry
-							v-for="file in folders"
-							v-bind:path="path"
-							v-bind:file="file"
-							v-bind:key="file.Key"
-						></file-entry>
-
-						<file-entry
-							v-for="file in singleFiles"
-							v-bind:path="path"
-							v-bind:file="file"
-							v-bind:key="file.Key"
-						></file-entry>
-					</tbody>
-				</table>
-			</div>
-
-			<div
-				v-if="fetchingFilesSpinner"
-				class="d-flex justify-content-center"
-			>
-				<div class="spinner-border"></div>
-			</div>
-
-			<div v-if="displayUpload" class="upload-help">
-				<svg
-					width="36"
-					height="36"
-					viewBox="0 0 36 36"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
+	<div class="row" v-on:click="closeModalDropdown">
+		<div class="col-sm-12">
+				<div
+					class="div-responsive"
+					v-cloak
+					@drop.prevent="upload"
+					@dragover.prevent
 				>
-					<path
-						d="M16.8616 1.02072L16.8554 1.01398L5.40027 13.3834L7.94585 16.1322L16.2 7.2192V26.2817H19.8V7.64972L27.6554 16.1321L30.201 13.3833L17.8069 0L16.8616 1.02072Z"
-						fill="#93A1AF"
-					/>
-					<path
-						d="M36 32.1127H0V36H36V32.1127Z"
-						fill="#93A1AF"
-					/>
-				</svg>
-				<h4 class="mt-4">Drop Files Here to Upload</h4>
-			</div>
+					<div class="row mb-2">
+						<div class="col-sm-12 col-md-4 col-xl-8 mb-3">
+							<bread-crumbs></bread-crumbs>
+						</div>
+
+						<div class="col-sm-12 col-md-4 col-xl-2 mb-3">
+							<button
+								class="btn btn-light btn-block"
+								v-on:click="toggleFolderCreationInput"
+								style="margin-right: 15px"
+							>
+							<svg width="17" height="16" viewBox="0 0 17 16" fill="none" class="mr-2" xmlns="http://www.w3.org/2000/svg">
+								<path d="M0.912109 1.92481C0.912109 1.1558 1.56545 0.5 2.41211 0.5H7.41211C7.70648 0.5 7.91211 0.724912 7.91211 0.962406C7.91211 1.78796 8.60191 2.42481 9.41211 2.42481H14.4121C15.2588 2.42481 15.9121 3.08061 15.9121 3.84962V14.0752C15.9121 14.8442 15.2588 15.5 14.4121 15.5H2.41211C1.56545 15.5 0.912109 14.8442 0.912109 14.0752V1.92481Z" fill="#768394" stroke="#7C8794"/>
+								<path d="M11.182 8.59043H5.79067M8.48633 5.89478V11.2861" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+								New Folder
+							</button>
+						</div>
+
+						<div class="col-sm-12 col-md-4 col-xl-2 mb-3">
+							<input
+								ref="fileInput"
+								type="file"
+								hidden
+								multiple
+								v-on:change="upload"
+							/>
+							<button
+								class="btn btn-primary btn-block"
+								v-on:click="buttonUpload"
+							>
+							<svg width="16" height="16" viewBox="0 0 16 16" fill="none" class="mr-2" xmlns="http://www.w3.org/2000/svg">
+								<path d="M7.49407 0.453655L7.49129 0.450658L2.40012 5.94819L3.53149 7.16987L7.20001 3.20854L7.20001 11.6808H8.80001V3.39988L12.2913 7.16983L13.4227 5.94815L7.91419 0L7.49407 0.453655Z" fill="white"/>
+								<path d="M16 14.2723H0V16H16V14.2723Z" fill="white"/>
+							</svg>
+								Upload File
+							</button>
+						</div>
+
+					</div>
+
+					<div>
+						<table class="table table-hover">
+							<file-browser-header></file-browser-header>
+
+							<tbody>
+								<tr
+									v-for="file in filesUploading"
+									v-bind:key="file.ETag"
+									scope="row"
+								>
+									<td class="upload-text">
+										<span>
+											<svg
+												width="21"
+												height="18"
+												viewBox="0 0 16 16"
+												fill="currentColor"
+												xmlns="http://www.w3.org/2000/svg"
+												class="bi bi-file-earmark ml-2 mr-1"
+											>
+												<path
+													d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"
+												></path>
+												<path
+													d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"
+												></path>
+											</svg>
+											{{ filename(file) }}
+										</span>
+									</td>
+									<td>
+										<div class="progress">
+											<div
+												class="progress-bar"
+												role="progressbar"
+												v-bind:style="{
+													width: `${file.progress}%`
+												}"
+											>
+												{{ file.progress }}%
+											</div>
+										</div>
+									</td>
+									<td>
+										<button
+											class="btn btn-danger btn-sm"
+											v-on:click="cancelUpload(file.Key)"
+										>
+											Cancel
+										</button>
+									</td>
+									<td>
+									</td>
+								</tr>
+
+								<tr v-if="path.length > 0">
+									<td class="px-3">
+										<router-link to="../">
+											<a
+												href="javascript:null"
+												v-on:click="back"
+												class="px-2 font-weight-bold"
+												>..</a
+											>
+										</router-link>
+									</td>
+								</tr>
+
+								<tr
+									v-if="
+										$store.state.files
+											.createFolderInputShow === true
+									"
+									class="new-folder-row"
+								>
+									<td span="3">
+										<input
+											class="form-control input-folder"
+											v-bind:class="{
+												'folder-input':
+													createFolderInput.length >
+														0 &&
+													!createFolderEnabled
+											}"
+											type="text"
+											placeholder="Name of the folder"
+											v-model="createFolderInput"
+											v-on:keypress.enter="createFolder"
+										/>
+									</td>
+									<td span="3">
+										<button
+											v-on:click="createFolder"
+											v-bind:disabled="
+												!createFolderEnabled
+											"
+											class="btn btn-primary btn-sm px-4"
+										>
+											Save Folder
+										</button>
+										<span class="mx-1"></span>
+										<button
+											class="btn btn-light btn-sm px-4"
+											v-on:click="cancelFolderCreation"
+										>
+											Cancel
+										</button>
+									</td>
+									<td span="3">
+									</td>
+									<td span="3">
+										<div
+											v-if="creatingFolderSpinner"
+											class="spinner-border"
+											role="status"
+										></div>
+									</td>
+								</tr>
+
+								<file-entry
+									v-for="file in folders"
+									v-bind:path="path"
+									v-bind:file="file"
+									v-bind:key="file.Key"
+								></file-entry>
+
+								<file-entry
+									v-for="file in singleFiles"
+									v-bind:path="path"
+									v-bind:file="file"
+									v-bind:key="file.Key"
+								></file-entry>
+							</tbody>
+						</table>
+					</div>
+
+					<div
+						v-if="fetchingFilesSpinner"
+						class="d-flex justify-content-center"
+					>
+						<div class="spinner-border"></div>
+					</div>
+
+					<div v-if="displayUpload" v-on:click="buttonUpload" class="upload-help">
+						<svg width="300" height="172" viewBox="0 0 300 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M188.5 140C218.047 140 242 116.047 242 86.5C242 56.9528 218.047 33 188.5 33C158.953 33 135 56.9528 135 86.5C135 116.047 158.953 140 188.5 140Z" fill="white"/>
+							<path d="M123.5 167C147.524 167 167 147.524 167 123.5C167 99.4756 147.524 80 123.5 80C99.4756 80 80 99.4756 80 123.5C80 147.524 99.4756 167 123.5 167Z" fill="white"/>
+							<path d="M71.5 49C78.9558 49 85 42.9558 85 35.5C85 28.0442 78.9558 22 71.5 22C64.0442 22 58 28.0442 58 35.5C58 42.9558 64.0442 49 71.5 49Z" fill="white"/>
+							<path d="M262.5 143C268.851 143 274 137.851 274 131.5C274 125.149 268.851 120 262.5 120C256.149 120 251 125.149 251 131.5C251 137.851 256.149 143 262.5 143Z" fill="white"/>
+							<path d="M185.638 64.338L191 57M153 109L179.458 72.7948L153 109Z" stroke="#276CFF" stroke-width="2" stroke-linecap="round"/>
+							<path d="M121.08 153.429L115 161M153 108L127.16 144.343L153 108Z" stroke="#276CFF" stroke-width="2" stroke-linecap="round"/>
+							<path d="M134 71L115 31M152 109L139 81L152 109Z" stroke="#FF458B" stroke-width="2" stroke-linecap="round"/>
+							<path d="M180.73 129.5L210 151M153 108L173.027 123.357L153 108Z" stroke="#FF458B" stroke-width="2" stroke-linecap="round"/>
+							<path d="M86.7375 77.1845L72 70M152 109L109.06 88.0667L152 109Z" stroke="#FFC600" stroke-width="2" stroke-linecap="round"/>
+							<path d="M152.762 109.227L244.238 76.7727" stroke="#00E567" stroke-width="2" stroke-linecap="round"/>
+							<path d="M154.5 104.5L111 131" stroke="#00E567" stroke-width="2" stroke-linecap="round"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M224 57H238V71H224V57Z" fill="#00E567"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M127 2H137V12H127V2Z" fill="#FF458B"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M150 166H156V172H150V166Z" fill="#FF458B"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M44 0H50V6H44V0Z" fill="#00E567"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M294 111H300V117H294V111Z" fill="#276CFF"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M0 121H6V127H0V121Z" fill="#276CFF"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M268 86H274V92H268V86Z" fill="#FFC600"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M28 91H46V109H28V91Z" fill="#FFC600"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M181 21H203V43H181V21Z" fill="#276CFF"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M154.958 55L179 79.0416V136H122V55H154.958Z" fill="#0218A7"/>
+							<path d="M146.5 80H136.5C135.119 80 134 81.1193 134 82.5C134 83.8807 135.119 85 136.5 85H146.5C147.881 85 149 83.8807 149 82.5C149 81.1193 147.881 80 146.5 80Z" fill="white"/>
+							<path d="M164.5 92H136.5C135.119 92 134 93.1193 134 94.5C134 95.8807 135.119 97 136.5 97H164.5C165.881 97 167 95.8807 167 94.5C167 93.1193 165.881 92 164.5 92Z" fill="white"/>
+							<path d="M164.5 104H136.5C135.119 104 134 105.119 134 106.5C134 107.881 135.119 109 136.5 109H164.5C165.881 109 167 107.881 167 106.5C167 105.119 165.881 104 164.5 104Z" fill="white"/>
+							<path d="M164.5 116H136.5C135.119 116 134 117.119 134 118.5C134 119.881 135.119 121 136.5 121H164.5C165.881 121 167 119.881 167 118.5C167 117.119 165.881 116 164.5 116Z" fill="white"/>
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M154.958 79.0416V55L179 79.0416H154.958Z" fill="#276CFF"/>
+						</svg>
+						<p class="drop-files-text mt-4 mb-0">Drop Files Here to Upload</p>
+					</div>
+				</div>
+			<file-modal
+				v-if="$store.state.files.modalPath !== null"
+			></file-modal>
+			<file-share-modal
+				v-if="$store.state.files.fileShareModal"
+			></file-share-modal>
 		</div>
-		<file-modal
-			v-if="$store.state.files.modalPath !== null"
-		></file-modal>
-		<file-share-modal
-			v-if="$store.state.files.fileShareModal"
-		></file-share-modal>
 	</div>
 </template>
 
@@ -388,7 +398,7 @@ export default {
 
 		displayUpload() {
 			return (
-				this.fetchingFilesSpinner === false && this.files.length === 0
+				this.fetchingFilesSpinner === false
 			);
 		}
 	},
