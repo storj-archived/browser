8ab4fe9da14d23d0d568adc073e12d75a5b7a718
Monty Anderson <work@montyanderson.net>
2021-06-30 17:42:14 +0100

    Hide dropzone when files already uploaded (#206)

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -751,7 +751,9 @@ export default {
 		},
 
 		displayUpload() {
-			return this.fetchingFilesSpinner === false;
+			return (
+				this.fetchingFilesSpinner === false && this.files.length === 0
+			);
 		}
 	},
 	watch: {
