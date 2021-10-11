c7def5f1ed4833401882f0f1ac8ff1891ab615ed
Monty Anderson <work@montyanderson.net>
2021-10-11 15:33:11 +0100

    prettierize

--- src/components/FileEntry.vue
+++ src/components/FileEntry.vue
@@ -138,10 +138,16 @@ a {
 			</span>
 		</td>
 		<td class="w-25">
-			<span v-if="fileTypeIsFile" aria-roledescription=file-size>{{ size }}</span>
+			<span v-if="fileTypeIsFile" aria-roledescription="file-size">{{
+				size
+			}}</span>
 		</td>
 		<td>
-			<span v-if="fileTypeIsFile" aria-roledescription=file-upload-date>{{ uploadDate }}</span>
+			<span
+				v-if="fileTypeIsFile"
+				aria-roledescription="file-upload-date"
+				>{{ uploadDate }}</span
+			>
 		</td>
 		<td class="text-right">
 			<div v-if="fileTypeIsFile" class="d-inline-flex">
